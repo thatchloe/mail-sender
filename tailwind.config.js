0f8cd672c8d819303e60bb668617911a420f5753
lương hoàng anh <anh@luongs-MacBook-Air.local>
2025-07-01 16:56:13 +0200

    fix: update Tailwind content paths for new main file and all .tsx/.jsx files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     './index.html',
     './main.jsx',
     './remixed-2c91cdd9-3.tsx',
+    './remixed-2c91cdd9-5.tsx',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './**/*.tsx',
+    './**/*.jsx',
   ],
   theme: {
     extend: {},
